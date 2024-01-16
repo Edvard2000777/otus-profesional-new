465acb492e2a5033b7398290496a107d3c87af67
0563 <0563166@gmail.com>
2024-01-16 20:15:54 +0400

    Hw33Https

---
+++ Hw30Https/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "2.6.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+}
+
+group = "org.example"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

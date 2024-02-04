a3664ea0bb66c634f204a707275ad75aba752571
0563 <0563166@gmail.com>
2024-02-04 13:54:39 +0400

    setting

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,4 +10,5 @@ rootProject.name = "otus-profesional-new"
 include( "hw01-gradle")
 include( "hw3-generici")
 include( "otus-unittest-homework")
-include( "hw11-web-server")
+include("Hw30Https")
+include("hw11-web-server")
